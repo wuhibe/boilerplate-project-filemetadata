6bd70f3cad13515d94457ac65ebd0faac85a0aca
Wuhibe Tamire <wuhibetamire710@gmail.com>
2023-01-05 06:33:02 +0000

    added a POST /api/fileanalyze route that responds with {name, type, size} JSON

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'),
+  (req, res) => {
+    let file = req.file;
+    let type = file.mimetype, size = file.size, name = file.originalname;
+    res.json({ name, type, size });
+  });
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
